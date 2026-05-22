8641917d075bdf192c5d1a2044c7ee3da9c00174
Stephanie Marys <139191572+Stephanie-marys@users.noreply.github.com>
2026-05-22 17:22:47 -0300

    Update Navbar: rename brand from CineLog to Cine Beth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <NavLink to="/" className="navbar-brand">
-        CINE<span>LOG</span>
+        CINE<span>BETH</span>
       </NavLink>
       <div className="navbar-links">
         <NavLink
